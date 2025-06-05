789b4ee6310c959a9d4951928c0e6914c4875323
Kanishka <akcadikari@std.appsc.sab.ac.lk>
2025-06-05 22:25:07 +0530

    day 19🙌

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -127,3 +127,40 @@ export async function getOrders(req,res) {
     
     
 }
+export async function updateOrderStatus(req,res){
+	if (!isAdmin(req)) {
+		res.status(403).json({
+			message: "You are not authorized to update order status",
+		});
+		return;
+	}
+	try{
+		const orderId = req.params.orderId;
+		const status = req.params.status;
+
+		await Order.updateOne(
+			{
+				orderId: orderId
+			},
+			{
+				status : status
+			}
+		)
+
+		res.json({
+			message: "Order status updated successfully",
+		});
+
+	}catch(e){
+		res.status(500).json({
+			message: "Failed to update order status",
+			error: e,
+		});
+		return;
+	}
+
+	
+
+	
+	
+}
