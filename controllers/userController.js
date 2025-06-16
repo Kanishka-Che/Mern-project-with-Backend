e24d23f7896b390b07a8b7013cd568b72225ea96
Kanishka <akcadikari@std.appsc.sab.ac.lk>
2025-06-16 19:03:03 +0530

    google login day 20-🙂

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
+import nodemailer from "nodemailer";
+
 dotenv.config();
 
 export function createUser(req,res){
@@ -21,8 +24,7 @@ export function createUser(req,res){
         }
     }
 
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10)//password hashing eka 10 parak karanna
-
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
 
     const user = new User({
         firstName : req.body.firstName,
@@ -52,18 +54,15 @@ export function loginUser(req,res){
     const email = req.body.email
     const password = req.body.password
 
-    User.findOne({email : email})//adala email eka userge req eken awa email ekama hoyala denna
-
-    .then(
+    User.findOne({email : email}).then(
         (user)=>{
             if(user == null){
                 res.status(404).json({
                     message : "User not found"
                 })
             }else{
-                const isPasswordCorrect = bcrypt.compareSync(password , user.password)/*password correct or not*/
-
-                if(isPasswordCorrect){//generate token and send it to the user
+                const isPasswordCorrect = bcrypt.compareSync(password , user.password)
+                if(isPasswordCorrect){
                     const token = jwt.sign(
                         {
                             email : user.email,
@@ -72,7 +71,7 @@ export function loginUser(req,res){
                             role : user.role,
                             img : user.img
                         },
-                        process.env.JWT_KEY//use jwt
+                        process.env.JWT_KEY
                     )
 
 
@@ -92,6 +91,179 @@ export function loginUser(req,res){
         }
     )
     
+}
+
+export async function loginWithGoogle(req,res){
+    const token = req.body.accessToken;
+    if(token == null){
+        res.status(400).json({
+            message: "Access token is required"
+        });
+        return;
+    }
+    const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    console.log(response.data);
+
+    const user = await User.findOne({
+        email: response.data.email
+    })
+    
+    if(user == null){
+        const newUser = new User(
+            {
+                email: response.data.email,
+                firstName: response.data.given_name,
+                lastName: response.data.family_name,
+                password: "googleUser",
+                img: response.data.picture 
+            }
+        )
+        await newUser.save();
+        const token = jwt.sign(
+            {
+                email: newUser.email,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                role: newUser.role,
+                img: newUser.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login successful",
+            token: token,
+            role: newUser.role
+        })
+
+    }else{
+
+        const token = jwt.sign(
+            {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+                img: user.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login successful",
+            token: token,
+            role: user.role
+        })
+
+    }
+
+}
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    host : 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: "malithdilshan27@gmail.com",
+        pass: "javyzfzydwsdrmbg"
+    }
+})
+export async function sendOTP(req,res){
+    //javy zfzy dwsd rmbg
+    const randomOTP = Math.floor(100000 + Math.random() * 900000);
+    const email = req.body.email;
+    if(email == null){
+        res.status(400).json({
+            message: "Email is required"
+        });
+        return;
+    
+    }
+    const user = await User.findOne({
+        email : email
+    })
+    if(user == null){
+        res.status(404).json({
+            message:"User not found"
+        })
+    }
+
+    //delete all otps
+    await OTP.deleteMany({
+        email: email
+    })
+
+    
+    const message = {
+        from : "malithdilshan27@gmail.com",
+        to: email,
+        subject : "Resetting password for crystal beauty clear.",
+        text : "This your password reset OTP : " + randomOTP
+    }
+
+    const otp = new OTP({
+        email : email,
+        otp : randomOTP
+    })
+    await otp.save()
+    transport.sendMail(message,(error,info)=>{
+            if(error){
+                res.status(500).json({
+                    message: "Failed to send OTP",
+                    error: error
+                });
+            }else{
+                res.json({
+                    message: "OTP sent successfully",
+                    otp: randomOTP
+                });
+            }
+        }
+    )
+}
+
+export async function resetPassword(req,res){
+    const otp  = req.body.otp
+    const email = req.body.email
+    const newPassword = req.body.newPassword
+    console.log(otp)
+    const response = await OTP.findOne({
+        email : email
+    })
+    
+    if(response==null){
+        res.status(500).json({
+            message : "No otp requests found please try again"
+        })
+        return
+    }
+    if(otp == response.otp){
+        await OTP.deleteMany(
+            {
+                email: email
+            }
+        )
+        console.log(newPassword)
+
+        const hashedPassword = bcrypt.hashSync(newPassword, 10)
+        const response2 = await User.updateOne(
+            {email : email},
+            {
+                password : hashedPassword
+            }
+        )
+        res.json({
+            message : "password has been reset successfully"
+        })
+    }else{
+        res.status(403).json({
+            meassage : "OTPs are not matching!"
+        })
+    }
+
 }
 
 export function isAdmin(req){
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, loginUser, loginWithGoogle, resetPassword, sendOTP } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/",createUser)//localhost:5000/users
 userRouter.post("/login", loginUser)//localhost:5000/users/login
+userRouter.post("/login/google", loginWithGoogle)
+
 
 
 export default userRouter;
+
+
+
+
+
+
